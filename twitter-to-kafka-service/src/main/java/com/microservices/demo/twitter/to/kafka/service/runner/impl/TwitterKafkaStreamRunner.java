8b20168b01e4395507cb3c609302612db2eca92a
Ahmed Khalil Khalef <46876471+a2khalil@users.noreply.github.com>
2021-06-09 23:10:43 +0200

    TwitterKafkaStatusListener

---
+++ twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/runner/impl/TwitterKafkaStreamRunner.java
@@ -0,0 +1,12 @@
+package com.microservices.demo.twitter.to.kafka.service.runner.impl;
+
+import com.microservices.demo.twitter.to.kafka.service.runner.StreamRunner;
+import twitter4j.TwitterException;
+
+public class TwitterKafkaStreamRunner implements StreamRunner {
+
+    @Override
+    public void start() throws TwitterException {
+
+    }
+}
