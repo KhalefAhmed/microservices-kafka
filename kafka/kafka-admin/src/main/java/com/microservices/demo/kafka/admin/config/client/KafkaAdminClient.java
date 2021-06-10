2d985a87644d9a4b83c11d94bd085844106395df
Ahmed Khalil Khalef <46876471+a2khalil@users.noreply.github.com>
2021-06-11 01:53:03 +0200

    createTopics method

--- kafka/kafka-admin/src/main/java/com/microservices/demo/kafka/admin/config/client/KafkaAdminClient.java
+++ kafka/kafka-admin/src/main/java/com/microservices/demo/kafka/admin/config/client/KafkaAdminClient.java
@@ -3,8 +3,10 @@ package com.microservices.demo.kafka.admin.config.client;
 import com.microservices.demo.config.KafkaConfigData;
 import com.microservices.demo.config.RetryConfigData;
 import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.CreateTopicsResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.retry.RetryContext;
 import org.springframework.retry.support.RetryTemplate;
 import org.springframework.stereotype.Component;
 
@@ -28,5 +30,23 @@ public class KafkaAdminClient {
         this.retryConfigData = retryConfigData;
         this.adminClient = client;
         this.retryTemplate = template;
+    }
+
+    public void createTopics(){
+        CreateTopicsResult createTopicsResult;
+        try {
+            createTopicsResult = retryTemplate.execute(this::doCreateTopics);
+        }
+        catch (Throwable e){
+            throw new RuntimeException("Reached max number of retry for creating kafka topic(s)");
+        }
+    }
+
+    private CreateTopicsResult doCreateTopics(RetryContext retryContext) {
+
+    }
+
+    public void checkTopicsCreated(){
+
     }
 }
