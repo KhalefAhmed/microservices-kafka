927dd8f4d34df6f910e7ec59152a816fcfed56d1
Ahmed Khalil Khalef <46876471+a2khalil@users.noreply.github.com>
2021-06-11 02:04:53 +0200

    CreateTopics

--- kafka/kafka-admin/src/main/java/com/microservices/demo/kafka/admin/config/client/KafkaAdminClient.java
+++ kafka/kafka-admin/src/main/java/com/microservices/demo/kafka/admin/config/client/KafkaAdminClient.java
@@ -2,17 +2,22 @@ package com.microservices.demo.kafka.admin.config.client;
 
 import com.microservices.demo.config.KafkaConfigData;
 import com.microservices.demo.config.RetryConfigData;
+import com.microservices.demo.kafka.admin.config.exception.KafkaClientException;
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.CreateTopicsResult;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.retry.RetryContext;
 import org.springframework.retry.support.RetryTemplate;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Component
 public class KafkaAdminClient {
-    private static final Logger LOG = LoggerFactory.getLogger(KafkaAdminClient.class);
+    private static final Logger logger = LoggerFactory.getLogger(KafkaAdminClient.class);
 
     private final KafkaConfigData kafkaConfigData;
 
@@ -38,12 +43,21 @@ public class KafkaAdminClient {
             createTopicsResult = retryTemplate.execute(this::doCreateTopics);
         }
         catch (Throwable e){
-            throw new RuntimeException("Reached max number of retry for creating kafka topic(s)");
+            throw new KafkaClientException("Reached max number of retry for creating kafka topic(s)");
         }
+        checkTopicsCreated();
     }
 
     private CreateTopicsResult doCreateTopics(RetryContext retryContext) {
+        List<String> topicNames = kafkaConfigData.getTopicNamesToCreate();
+        logger.info("Creating {} topic(s), attempt {}", topicNames.size(), retryContext.getRetryCount() );
+        List<NewTopic> kafkaTopics = topicNames.stream().map(topic ->  new NewTopic(
+                topic.trim(),
+                kafkaConfigData.getNumOfPartitions(),
+                kafkaConfigData.getReplicationFactor()
+        )).collect(Collectors.toList());
 
+        return adminClient.createTopics(kafkaTopics);
     }
 
     public void checkTopicsCreated(){
